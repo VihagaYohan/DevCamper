4a61a0a3e33dba8fc51a9a9407a73656c3d12b81
VihagaYohan <vihagayohan94@gmail.com>
2021-05-03 15:05:33 +0530

    Initialise mongoDB connection and implement unhandled promise rejection

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require('morgan')
+const morgan = require("morgan");
+
+// import DB connection
+const connectDB = require("./config/db");
 
 // import middle-ware
-const logger = require('./middleware/logging')
+const logger = require("./middleware/logging");
 
 // load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// connect to database
+connectDB();
+
 // initialize middleware
 //app.use(logger)
 
 // dev logging middle-ware
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // route files
-const bootcamps = require('./routes/bootcamps')
+const bootcamps = require("./routes/bootcamps");
 
 // mount route files
-app.use('/api/v1/bootcamps',bootcamps)
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server is running on ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
   );
+});
+
+// handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error : ${err.message}`);
+  // close server and exit process
+  server.close(() => process.exit(1));
 });
